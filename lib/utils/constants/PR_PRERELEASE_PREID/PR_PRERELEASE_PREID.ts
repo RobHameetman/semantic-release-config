df7e08ddb187e03599e4d6b0d7d93ff26d0af295
Robert Hameetman <engineering@robhameetman.com>
2023-11-22 13:04:58 -0600

    fix: Revert preid generification

--- lib/utils/constants/PR_PRERELEASE_PREID/PR_PRERELEASE_PREID.ts
+++ lib/utils/constants/PR_PRERELEASE_PREID/PR_PRERELEASE_PREID.ts
@@ -8,8 +8,8 @@ import { env } from '@utils/functions/environment/env';
  * The preid used for PR prerelease versions. You can override the default value
  * with the `RELEASE_PR_PRERELEASE_PREID` environment variable.
  *
- * @defaultValue - `${PR_PRERELEASE_TYPE}.${PR_NUMBER}`
+ * @defaultValue - `${PR_PRERELEASE_TYPE}.${PR_NUMBER}.${COMMIT_SHA_SHORT}.${DATE_HASH}`
  */
 export const PR_PRERELEASE_PREID =
 	env('RELEASE_PR_PRERELEASE_PREID') ||
-	`${PR_PRERELEASE_TYPE}.${PR_NUMBER}`;
+	`${PR_PRERELEASE_TYPE}.${PR_NUMBER}.${COMMIT_SHA_SHORT}.${DATE_HASH}`;
