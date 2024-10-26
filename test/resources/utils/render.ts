dc08d2deb2a528aad02e83c0272b43b673010241
Robert Hameetman <engineering@robhameetman.com>
2024-10-26 11:59:21 -0500

    chore: Remove debugging logs

--- test/resources/utils/render.ts
+++ test/resources/utils/render.ts
@@ -1,10 +1,5 @@
 import template from 'lodash/template';
 import { fakeNextRelease } from '../fakes/fakeNextRelease';
 
-export const render = ($template: string, options: Record<string, unknown>) => {
-	console.log($template);
-	const result = template($template)(fakeNextRelease(options));
-
-
-	return result;
-}
+export const render = ($template: string, options: Record<string, unknown>) =>
+	template($template)(fakeNextRelease(options));
