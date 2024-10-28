2e5c6e98c772f40c6afccc13d21b58d32f02a493
Robert Hameetman <engineering@robhameetman.com>
2024-10-27 23:14:04 -0500

    fix: Deprecate previous prerelease versions

--- lib/templates/ranges/$supportLatestPrerelease/$supportLatestPrerelease.ts
+++ lib/templates/ranges/$supportLatestPrerelease/$supportLatestPrerelease.ts
@@ -1,9 +1,10 @@
 import { isArray } from '@rob.hameetman/type-guards';
+import { $ifPrerelease } from '@/templates/conditional';
 import { major } from '@/templates/versions/major/major';
 import { minor } from '@/templates/versions/minor/minor';
 import { patch } from '@/templates/versions/patch/patch';
 import { preid } from '@/templates/versions/prerelease/preid';
-import { $ifPrerelease } from '@/templates/conditional';
+import { prerelease } from '@/templates/versions/prerelease/prerelease';
 import { prereleaseBack } from '@/templates/versions/prerelease/prereleaseBack';
 
 /**
@@ -20,7 +21,7 @@ import { prereleaseBack } from '@/templates/versions/prerelease/prereleaseBack';
 export const $supportLatestPrerelease = ($preids: string | ReadonlyArray<string> = preid()) =>
 	$ifPrerelease(
 		isArray($preids)
-			? $preids.map(($preid) => `'>= ' + ${major()} + '.' + ${minor()} + '.' + ${patch()} + '-' + ${String($preid).startsWith('nextRelease') ? $preid : `'${$preid}'`} + '.' + (${prereleaseBack()} === '1' ? '1' : '2') + ' < ' + nextRelease.version`).join(' || ')
+			? $preids.map(($preid) => `'>= ' + ${major()} + '.' + ${minor()} + '.' + ${patch()} + '-' + ${String($preid).startsWith('nextRelease') ? $preid : `'${$preid}'`} + '.' + (${prereleaseBack()} === '1' ? '1' : '2') + ' < ' + ${major()} + '.' + ${minor()} + '.' + ${patch()} + '-' + ${String($preid).startsWith('nextRelease') ? $preid : `'${$preid}'`} + '.' + ${prerelease()}`).join(' || ')
 			: `'>= ' + ${major()} + '.' + ${minor()} + '.' + ${patch()} + '-' + ${String($preids).startsWith('nextRelease') ? $preids : `'${$preids}'`} + '.' + (${prereleaseBack()} === '1' ? '1' : '2') + ' < ' + nextRelease.version`,
 			'">= " + nextRelease.version + " < " + nextRelease.version',
 	);
